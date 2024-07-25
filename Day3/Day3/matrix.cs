f7176189e797fe71825627564c729509daf8ae8c
Athira Muraleedharan <251210@ust.com>
2024-07-25 19:47:16 +0530

    Day4

--- Day3/Day3/matrix.cs
+++ Day3/Day3/matrix.cs
@@ -1,13 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Principal;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Day3
 {
-    internal class matrixsum
+    internal class matrix
     {  
+        public static void printmatrix(int[,] matrix)
+        {
+            
+            //print result
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                Console.WriteLine("\n");
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+
+                    Console.Write(matrix[i, j] + "\t");
+
+                }
+            }
+        }
         //read matrix from user
         public static void readMatrix(ref int[,] matrix)
         {
@@ -38,20 +54,20 @@ namespace Day3
 
                 }
             }
-            Console.WriteLine("Result Matrix : ");
-            //print result
+            Console.WriteLine("Result of Sum Matrix : ");
+            printmatrix(result);
+
+            //multiplication matrix
             for (int i = 0; i < result.GetLength(0); i++)
             {
-                Console.WriteLine("\n");
                 for (int j = 0; j < result.GetLength(1); j++)
                 {
-                    
-                    Console.Write(result[i,j]+"\t");
+                    result[i, j] = matrix1[i, j] * matrix2[i, j];
 
                 }
             }
-
-
+            Console.WriteLine("Result of Multiplication Matrix : ");
+            printmatrix(result);
 
         }
     }
